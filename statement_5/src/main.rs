3473b8e3d2112679fd1b43bb6882c11e130519ad
LakshmiBellapukonda <madhuribellapukonda000@gmail.com>
2024-08-08 16:25:15 +0530

    Add files via upload

---
+++ statement_5/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut count =0;
+
+    loop {
+        count += 1;
+        println!("Count is: {}", count);
+
+        if count >= 5 {
+            break;
+        }
+    }
+     println!("Loop has ended: {}", count);
+}
---
+++ statement_5/target/debug/statement_5.d
@@ -0,0 +1,1 @@
+C:\Users\Dell\Desktop\rust\statement_5\target\debug\statement_5.exe: C:\Users\Dell\Desktop\rust\statement_5\src\main.rs
