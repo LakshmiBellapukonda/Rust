5eeee4fed9fd7317d38878a5ceee49e479c69d73
LakshmiBellapukonda <madhuribellapukonda000@gmail.com>
2024-08-08 16:24:24 +0530

    Add files via upload

---
+++ statement_4/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut counter =0;
+
+    while counter < 5{
+        println!("counter is: {}", counter);
+        counter += 1;
+
+    }
+    println!("Loop ended. Final counter value is: {}", counter);
+}
---
+++ statement_4/target/debug/statement_4.d
@@ -0,0 +1,1 @@
+C:\Users\Dell\Desktop\rust\statement_4\target\debug\statement_4.exe: C:\Users\Dell\Desktop\rust\statement_4\src\main.rs
